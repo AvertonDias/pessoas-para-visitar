a8101c7c0bbf6d5ad5b9dbfb54779c6b9c9184a7
Ton Aplicativos <tonaplicativos@gmail.com>
2026-03-28 15:48:24 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,6 +7,6 @@ export const firebaseConfig = {
   appId: "1:190192495370:web:74f4e432061c294dec68f3",
   apiKey: "AIzaSyDRUAVStTAKWeaGOUUDYGB3rChEDoV_r6Q",
   authDomain: "studio-2478759487-b93de.firebaseapp.com",
-  messagingSenderId: "190192495370",
-  storageBucket: "studio-2478759487-b93de.appspot.com"
+  measurementId: "",
+  messagingSenderId: "190192495370"
 };
