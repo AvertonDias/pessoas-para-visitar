5af15c4966ed05547e095b11783f83b63aec622a
Ton Aplicativos <tonaplicativos@gmail.com>
2026-03-28 15:46:12 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,11 +3,10 @@
 // Using an empty object will not throw an error, but it will not work with
 // any Firebase services.
 export const firebaseConfig = {
-  apiKey: "AIzaSyC4MP_41p5_VwEI-pC9h-sL1N3bSgoj5Vw",
-  authDomain: "listanomes-pwa.firebaseapp.com",
-  projectId: "listanomes-pwa",
-  storageBucket: "listanomes-pwa.appspot.com",
-  messagingSenderId: "95199427653",
-  appId: "1:95199427653:web:715a201b13d395d985a1a1",
-  measurementId: "G-9Q2Z1X0P3L"
+  projectId: "studio-2478759487-b93de",
+  appId: "1:190192495370:web:74f4e432061c294dec68f3",
+  apiKey: "AIzaSyDRUAVStTAKWeaGOUUDYGB3rChEDoV_r6Q",
+  authDomain: "studio-2478759487-b93de.firebaseapp.com",
+  messagingSenderId: "190192495370",
+  storageBucket: "studio-2478759487-b93de.appspot.com"
 };
