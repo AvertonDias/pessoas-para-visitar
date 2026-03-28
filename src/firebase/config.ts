fc2cfde1b4c26bf4335cc4fdd9328d0621ead0a5
Averton Silva Dias <verton3@gmail.com>
2026-03-28 15:39:25 +0000

    ?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,4 +2,12 @@
 // The Firebase configuration will be injected by the App Hosting backend.
 // Using an empty object will not throw an error, but it will not work with
 // any Firebase services.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4MP_41p5_VwEI-pC9h-sL1N3bSgoj5Vw",
+  authDomain: "listanomes-pwa.firebaseapp.com",
+  projectId: "listanomes-pwa",
+  storageBucket: "listanomes-pwa.appspot.com",
+  messagingSenderId: "95199427653",
+  appId: "1:95199427653:web:715a201b13d395d985a1a1",
+  measurementId: "G-9Q2Z1X0P3L"
+};
