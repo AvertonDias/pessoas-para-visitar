79fb32105acdd03b8b6a3d874b2199bda8510a7a
Ton Aplicativos <tonaplicativos@gmail.com>
2026-03-28 16:17:16 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
 // Using an empty object will not throw an error, but it will not work with
 // any Firebase services.
 export const firebaseConfig = {
-  "projectId": "studio-2478759487-b93de",
-  "appId": "1:190192495370:web:74f4e432061c294dec68f3",
-  "apiKey": "AIzaSyDRUAVStTAKWeaGOUUDYGB3rChEDoV_r6Q",
-  "authDomain": "studio-2478759487-b93de.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "190192495370"
+  apiKey: "AIzaSyDRUAVStTAKWeaGOUUDYGB3rChEDoV_r6Q",
+  authDomain: "studio-2478759487-b93de.firebaseapp.com",
+  projectId: "studio-2478759487-b93de",
+  storageBucket: "studio-2478759487-b93de.firebasestorage.app",
+  messagingSenderId: "190192495370",
+  appId: "1:190192495370:web:74f4e432061c294dec68f3"
 };
